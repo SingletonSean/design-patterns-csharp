e2f265f8e031b5d3d94f0138e9d558c4c3acf2dd
SingletonSean <bballheat6@gmail.com>
2021-02-01 15:32:15 -0500

    Add composite demo

---
+++ Composite/Models/AudioTimeline.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite.Models
+{
+    public class AudioTimeline
+    {
+        private readonly List<TimeSpan> _cuts;
+
+        public string Title { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public IEnumerable<TimeSpan> Cuts => _cuts;
+
+        public AudioTimeline(TimeSpan startTime, TimeSpan endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+
+            _cuts = new List<TimeSpan>();
+        }
+
+        public void AddCut(TimeSpan cut)
+        {
+            _cuts.Add(cut);
+        }
+
+        public IEnumerable<string> ToOutputLines()
+        {
+            List<string> lines = new List<string>();
+
+            lines.Add($"TITLE: {Title}");
+            lines.Add($"START TIME: {StartTime}");
+            lines.Add($"END TIME: {EndTime}");
+            lines.Add("CUTS:");
+            _cuts.ForEach(t => lines.Add(t.ToString()));
+
+            return lines;
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,22 @@
+﻿using Composite.Models;
+using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AudioTimeline timeline = new AudioTimeline(TimeSpan.Zero, TimeSpan.FromSeconds(10));
+            timeline.Title = "Main Timeline";
+            timeline.AddCut(TimeSpan.FromSeconds(2));
+            timeline.AddCut(TimeSpan.FromSeconds(4));
+            timeline.AddCut(TimeSpan.FromSeconds(5));
+
+            foreach (string line in timeline.ToOutputLines())
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
